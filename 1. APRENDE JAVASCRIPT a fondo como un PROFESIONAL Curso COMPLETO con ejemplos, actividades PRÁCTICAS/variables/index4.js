00e39bf9de7216a06c072d16d1648f7f15f04e05
NextCodigos <nebulanemesis376@gmail.com>
2024-06-12 12:07:55 +0200

    Añado apuntes tipos de variables

---
+++ 1. APRENDE JAVASCRIPT a fondo como un PROFESIONAL Curso COMPLETO con ejemplos, actividades PRÁCTICAS/variables/index4.js
@@ -0,0 +1,18 @@
+let nombre="Jab";
+let NOMBRE="Jab";
+let Nombre="Jab";
+let $Nombre="Jab";
+let _Nombre="Jab";
+let miNombre="Jab";
+let miNombreAhora="Jab";
+// SnakeCase
+let MiNombreAhora="Jab";
+// kebab_case
+let mi_nombre="Jab";
+// UPPER_CASE
+let MI_NOMBRE="Jab";
+
+// console.log(miNombre)
+// Distingue entre minúsculas y mayúsculas.
+// Sintais cameCase.
+// Solo pueden empezar con una letra,dolar o guion bajo, Sin espacios,ñ,acentos o smbolos raros.

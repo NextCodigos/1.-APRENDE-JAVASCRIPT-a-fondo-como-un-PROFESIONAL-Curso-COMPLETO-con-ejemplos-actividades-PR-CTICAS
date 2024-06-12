db9855463b6e346cf8e2734efb76dc4b0b11b45b
NextCodigos <nebulanemesis376@gmail.com>
2024-06-12 10:48:33 +0200

    Añadiendo eejemplos principio del video

---
+++ 1. APRENDE JAVASCRIPT a fondo como un PROFESIONAL Curso COMPLETO con ejemplos, actividades PRÁCTICAS/variables/index.js
@@ -0,0 +1,7 @@
+console.log(vivo);//undefined
+console.log(edad);//Uncaught ReferenceError: can't access lexical declaration 'edad' before initialization
+let edad=18;
+edad=19;
+var vivo=true;
+vivo=false;
+const NOMBRE="Jab";
---
+++ 1. APRENDE JAVASCRIPT a fondo como un PROFESIONAL Curso COMPLETO con ejemplos, actividades PRÁCTICAS/variables/index2.js
@@ -0,0 +1,17 @@
+console.log(ojos);//undefined
+
+let edad=18;
+edad=19;
+var vivo=true;
+vivo=false;
+const NOMBRE="Jab";
+
+// Todas variables entre {} solo tienen valor dentro de ellas
+if (true){
+    var ojos=2;
+}
+
+if (true) {
+    let edad=18;
+    console.log(ojos);//2
+}

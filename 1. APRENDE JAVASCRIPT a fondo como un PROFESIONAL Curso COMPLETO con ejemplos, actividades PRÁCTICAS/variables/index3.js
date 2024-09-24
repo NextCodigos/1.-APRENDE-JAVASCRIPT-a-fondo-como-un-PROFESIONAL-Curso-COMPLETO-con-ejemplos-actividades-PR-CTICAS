f93dd16093a2576c241c875c9ac9abeb1b003092
NextCodigos <nebulanemesis376@gmail.com>
2024-09-24 22:07:40 +0200

    Alñadiendo opciones

--- 1. APRENDE JAVASCRIPT a fondo como un PROFESIONAL Curso COMPLETO con ejemplos, actividades PRÁCTICAS/variables/index3.js
+++ 1. APRENDE JAVASCRIPT a fondo como un PROFESIONAL Curso COMPLETO con ejemplos, actividades PRÁCTICAS/variables/index3.js
@@ -1,16 +1,18 @@
 saludar();
-let nombre="Javier";
-console.log(nombre)//Javier
+let nombre="Beny y Oleg";
+console.log(nombre)//Beny y Oleg
 const OJOS=1;
+let gusanos="Brocheta"
 
 // Variables de ambito local entre {}
-function saludar() {
-    let nombre="Jab";
+function saludar(gusanos) {
+    let nombre="BENI";
     const OJOS=2;
     var edad=18;
-    console.log(nombre)//Jab
+    console.log(nombre)//BENI
+    
 }
-
+saludar(gusanos);//BENI  
 // Salida por consola
 // jab
 // javier
